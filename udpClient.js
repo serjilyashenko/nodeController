83d81e34beada9eeba4f7dbf32220ca9f61d9a37
sky <sky.ilyashenko@gmail.com>
2016-02-22 10:15:50 +0300

    first udpClient

--- udpClient.js
+++ udpClient.js
@@ -3,11 +3,20 @@ var host = "192.168.0.77";
 
 var dgram = require('dgram');
 // var message = new Buffer("Hello UDP Server :)");
-var message = new Buffer("Hello UDP Server :)");
+
+// Creating hdl telegram
+var senderIp = new Buffer([192,168,0,102]);
+var hdlMiracle = new Buffer("HDLMIRACLE");
+// data over rs485
+var leaderCode = [0xaa, 0xaa];
+var hdlTelegram = leaderCode.concat([0xbb, 0xbb]);
+hdlBuffer = new Buffer(hdlTelegram);
+
+console.log(Buffer.concat([senderIp, hdlMiracle, hdlBuffer]));
 
 var client = dgram.createSocket('udp4');
-client.send(message, 0 , message.length, port, host, function(err, bytes){
-	if (err) throw err;
-	console.log("UDP message sent to " + host + ":" + port);
-	client.close();
-} );
+// client.send(message, 0 , message.length, port, host, function(err, bytes){
+// 	if (err) throw err;
+// 	console.log("UDP message sent to " + host + ":" + port);
+// 	client.close();
+// } );
